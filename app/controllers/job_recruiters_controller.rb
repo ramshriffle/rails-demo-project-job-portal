2f646b27758832e82a192727e4c0966ef29886dc
hp <ramkrishnak@shriffle.com>
2023-09-07 10:57:06 +0530

    only recruiter accept and reject the job application

--- app/controllers/job_recruiters_controller.rb
+++ app/controllers/job_recruiters_controller.rb
@@ -2,17 +2,15 @@ class JobRecruitersController < ApplicationController
   before_action :check_job_recruiter, only: [:update]
   before_action :set_param, only: [:view_applied_job, :update]
 
-
-
   def view_applied_job
     begin
-    # @job= @current_user.jobs.find(params[:id])
-    applied_applications = @job.user_applications
-    if ! applied_applications.blank?
-      render json: applied_applications
-    else
-      render json: "Nobody apply for job"
-    end
+      @job= @current_user.jobs.find(params[:id])
+      applied_applications = @job.user_applications
+      if ! applied_applications.blank?
+        render json: applied_applications
+      else
+        render json: "Nobody apply for job"
+      end
     rescue
       render json: "Job Not Found"
     end
@@ -23,19 +21,15 @@ class JobRecruitersController < ApplicationController
     render json: applications
   end
 
-  def update  
-    # seeker_application = UserApplication.find(params[:id])
-    # if seeker_application.update(set_param)
-    #   render json: "Application Updated"
-    # else
-    #   render json: seeker_application.errors.full_messages
-    # end
-
-    job_application= @job.user_applications.find(params[:id])
-    if job_application.update(recruiter_param)
-      render json: "Application Updated"
+  def update
+    if @current_user==@application.job.user
+      if @application.update(recruiter_param)
+        render json: {message: "Application Updated",data: @application}
+      else
+        render json: seeker_application.errors.full_messages
+      end
     else
-      render json: seeker_application.errors.full_messages
+      render json: "You not permission to accept and reject the job"
     end
   end
   
@@ -47,9 +41,9 @@ class JobRecruitersController < ApplicationController
   private
   def set_param
     begin
-      @job= @current_user.jobs.find(params[:id])
+      @application= UserApplication.find(params[:id])
     rescue
-      render json: "Job not Found"
+      render json: "application not Found"
     end
   end
 end
